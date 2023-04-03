572ba9adcb6bd70e49c36220dcbdb5bddfafcef2
klozz <ya.slava2323yaru@gmail.com>
2023-04-03 20:14:28 +0300

    test

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+import java.sql.*;
+public class Application {
+    public static void main(String[] args) throws SQLException {
+
+        final String user = "postgres";
+        final String password = "slavakakraft228";
+        final String url = "jdbc:postgresql://localhost:5432/skypro";
+    }
+}
